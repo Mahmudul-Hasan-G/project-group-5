937323474e3f5027be8adebcbfecd7a1c1e4b6ea
anu muley <annusannu@gmail.com>
2023-08-22 11:44:09 +0200

    anu

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul className="flex text-center justify-between bg-lime-300 p-2 text-2xl mt-4">
+                <li>Home</li>
+                <li>Politics</li>
+                <li>Sports</li>
+                <li>Entertainment</li>
+                <li>About Us</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Navbar;
