3f6fbe09f86718a3ba4fcad399ebfbbd1caa7b9a
Phoenix <Phoenix@example.com>
2023-08-23 13:55:18 +0200

    entertainment

--- src/components/EntertainmentNews/EntertainmentNews.jsx
+++ src/components/EntertainmentNews/EntertainmentNews.jsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react";
 import Heading from "../Heading/Heading";
 import Navbar from "../Navbar/Navbar";
-
+import News from "../News/News";
 
 const EntertainmentNews = () => {
+    const [enews, setEnews] = useState([]);
+
+    useEffect(() => {
+        fetch("entertainment.json")
+        .then(res => res.json())
+        .then(data => setEnews(data))
+    }, [])
     return (
         <div>
             <Heading />
             <Navbar />
+            {
+                enews.map(news =><News 
+                key={news.id}
+                news={news}
+                />) 
+            }
         </div>
     );
 };
