59b10f41682cde3cf7611e4e88f48b525c9597e2
Mahmudul-Hasan-G <shantohasan1983@gmail.com>
2023-09-10 12:51:25 +0200

    hasan

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,5 +1,4 @@
-import Anu from "../Anu/Anu";
-import Diana from "../Diana/Diana";
+
 import Hasan from "../Hasan/Hasan";
 import Heading from "../Heading/Heading";
 import Navbar from "../Navbar/Navbar";
@@ -11,8 +10,7 @@ const AboutUs = () => {
             <Heading />
             <Navbar />
             <Hasan />
-            <Diana />
-            <Anu />
+
         </div>
     );
 };
